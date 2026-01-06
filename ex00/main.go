c16dbb34928ebaf96dc8169de76d5ebdb3d940e0
TaisukeFujise <tafujise3502@icloud.com>
2026-01-07 01:53:49 +0900

    totyu-

--- ex00/main.go
+++ ex00/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	for _, fileName := range os.Args {
+		err := readFile(fileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s", err)
+			os.Exit(1)
+		}
+	}
+}
+
+func readFile(fileName string) error {
+	f, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Scanerr: %s", err)
+	}
+	return nil
+}
