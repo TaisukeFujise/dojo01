daca0a0ac128cc4743f446874d6f7d97e241d875
TaisukeFujise <tafujise3502@icloud.com>
2026-01-07 10:59:34 +0900

    Add function when there are no args

--- ex00/main.go
+++ ex00/main.go
@@ -1,16 +1,33 @@
+/*
+Ft_cat imitates the UNIX cat command.
+It concatenates files and print on the standard output.
+Unlike the UNIX cat, it doesn't have any options.
+
+Usage:
+
+	ft_cat [FILE]
+*/
 package main
 
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	for _, fileName := range os.Args {
+	if len(os.Args) < 2 {
+		err := conCat(os.Stdout, os.Stdin)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error: %s\n", err)
+			os.Exit(1)
+		}
+	}
+	for _, fileName := range os.Args[1:] {
 		err := readFile(fileName)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %s", err)
+			fmt.Fprintf(os.Stderr, "error: %s\n", err)
 			os.Exit(1)
 		}
 	}
@@ -22,12 +39,20 @@ func readFile(fileName string) error {
 		return err
 	}
 	defer f.Close()
-	scanner := bufio.NewScanner(f)
+	err = conCat(os.Stdout, f)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func conCat(w io.Writer, r io.Reader) error {
+	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		fmt.Fprintln(w, scanner.Text())
 	}
 	if err := scanner.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "Scanerr: %s", err)
+		return fmt.Errorf("scanner: %s", err)
 	}
 	return nil
 }
